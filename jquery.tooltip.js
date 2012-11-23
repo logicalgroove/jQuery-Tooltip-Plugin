641ec25bb23f79e6e2c6514bb780db4fa2fc0f1f
LogicalGroove <webodessa@gmail.com>
2012-11-23 18:39:46 +0000

    Now the height of the tooltip is not fixed, that allow use mutli lines.

--- jquery.tooltip.js
+++ jquery.tooltip.js
@@ -1,26 +1,27 @@
 /*
-* jQuery Tooltip Plugin 0.1
+* jQuery Tooltip Plugin 0.2
 * https://github.com/logicalgroove/jQuery-Tooltip-Plugin
 * A simple jquery tooltip plugin
 * Author: Alexander Lopez
 */
 
-
 (function($){
 $.fn.tooltip = function(){
   return this.each(function () {
     $(this).mouseover(function(e) {
+      var tipWidth = $('#tooltip').outerWidth();
+      var tipHeight = $('#tooltip').outerHeight();
       var tip = $(this).attr('title');
       $(this).attr('title','');
       $('body').append('<div id="tooltip"><div class="tooltip-caption">'+ tip +'</div><div class="tooltip-bg"></div></div>')
-      var tipWidth = $('#tooltip').outerWidth()
-      $('#tooltip').css('top', e.pageY - 30 );
-      $('#tooltip').css('left', e.pageX - (tipWidth/2) );
+      $('#tooltip').css('top', e.pageY - (tipHeight + 4) + 'px');
+      $('#tooltip').css('left', e.pageX - (tipWidth/2) + 'px');
       $('#tooltip').fadeIn('500');
     }).mousemove(function(e) {
-      var tipWidth = $('#tooltip').outerWidth()
-      $('#tooltip').css('top', e.pageY - 30 );
-      $('#tooltip').css('left', e.pageX - (tipWidth/2) );
+      var tipWidth = $('#tooltip').outerWidth();
+      var tipHeight = $('#tooltip').outerHeight();
+      $('#tooltip').css('top', e.pageY - (tipHeight + 4) + 'px' );
+      $('#tooltip').css('left', e.pageX - (tipWidth/2) + 'px');
     }).mouseout(function() {
       $(this).attr('title',$('.tooltip-caption').html());
       $('body').children('div#tooltip').remove();
